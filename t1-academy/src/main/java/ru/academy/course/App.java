3c3947d0ed56fd8e2fe87793d2e210b949a03cc2
krause214 <none>
2025-10-03 23:58:47 +0400

    -complete concurrency task

--- t1-academy/src/main/java/ru/academy/course/App.java
+++ t1-academy/src/main/java/ru/academy/course/App.java
@@ -1,17 +1,30 @@
 package ru.academy.course;
 
-import ru.academy.course.entity.Position;
-import ru.academy.course.entity.Worker;
-import ru.academy.course.stream.StreamTask;
+import ru.academy.course.concurrency.ThreadPoolImplementation;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        ThreadPoolImplementation threadPool = new ThreadPoolImplementation(4);
+
+        addTasks(threadPool, 10);
+
+        Thread.sleep(10000);
+
+        addTasks(threadPool, 15);
+
+        Thread.sleep(2000);
+
+        threadPool.shutdown();
+        threadPool.awaitTermination();
+
+        System.out.println("MAIN PROCESS ENDS");
+
         //TestRunner.runTests(FountainTest.class);
 
-        List<Integer> integerList = List.of(1, 10, 12, 5, 64, 22, 123, 1, 123);
+        /*List<Integer> integerList = List.of(1, 10, 12, 5, 64, 22, 123, 1, 123);
         List<Worker> workerList = List.of(
                 new Worker("Alex", 70, Position.ENGINEER),
                 new Worker("Fedor", 21, Position.MANAGER),
@@ -36,6 +49,23 @@ public class App {
         System.out.println(StreamTask.getMostLongWord(stringList));
         System.out.println(StreamTask.getWordCountedMap(stroka));
         StreamTask.stringPrinterByLength(stringList);
-        System.out.println(StreamTask.getMostLongInnerWord(sentenceList));
+        System.out.println(StreamTask.getMostLongInnerWord(sentenceList));*/
+    }
+
+    private static void addTasks(ThreadPoolImplementation threadPool, int amount) {
+        for (int i = 0; i < amount; i++) {
+            int taskNumber = i;
+            Random random = new Random();
+            threadPool.execute(
+                    () -> {
+                        System.out.println("execution task number " + taskNumber);
+                        try {
+                            Thread.sleep(1000 + random.nextInt() % 500);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+            );
+        }
     }
 }
